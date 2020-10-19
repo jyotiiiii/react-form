b8c306cb486d64ff2b50d5419f029d747882c008
Jyoti Sharma <jyoti.sharma@bbc.co.uk>
2020-10-19 12:10:03 +0100

    create basic form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>react-hook-form</h1>
+      <form autoComplete="off">
+        <label>Username</label>
+        <input type="text" name="username" />
+
+        <label>Age</label>
+        <input type="number" name="age" />
+
+        <span>
+          <input type="checkbox" name="remember" />
+          <label>Remember Me</label>
+        </span>
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
