46b599e1f422e3d47dfec3e973dd0a88cecd39be
Jyoti Sharma <jyoti.sharma@bbc.co.uk>
2020-10-20 11:36:13 +0100

    basic form

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const [formData, setFormData] = useState({
+    username: "",
+    age: 0,
+    remember: false,
+  });
+
+  function onSubmit(event) {}
+
   return (
     <div>
       <h1>react-hook-form</h1>
-      <form autoComplete="off">
+      <form autoComplete="off" onSubmit={onSubmit}>
         <label>Username</label>
         <input type="text" name="username" />
 
